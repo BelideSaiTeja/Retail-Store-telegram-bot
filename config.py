e48f0584131ad126e542b4da3ecf9c6cead70027
BelideSaiTeja <94059134+BelideSaiTeja@users.noreply.github.com>
2023-05-06 15:06:40 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('5903714552:AAE8d_0OeOf4UbfIH0XP7DS4EfOGEdgxyZs')
+api_secret = os.getenv('yJZntIAJkhmgAklTMOUcPay7TMo')
+api_key = os.getenv('576366756752361')
+FAUNA_KEY = os.getenv('363864306562891856')
